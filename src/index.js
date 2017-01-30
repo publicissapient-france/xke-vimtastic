19ec7be0ab6fd9268d40f23897df501b9f422680
Peter Onneby <ponneby@xebia.fr>
2017-01-30 18:00:44 +0100

    Extract monuments.js
    New buffersplit :vnew
    <tab>
    cut functions (n+dd)
    <tab>
    paste (p)
    :w src/monuments.js
    Export functions (module.exports)
    <tab>
    require functions using destructration
    :w

--- src/index.js
+++ src/index.js
@@ -2,41 +2,23 @@ const path = require('path');
 const fs = require('fs');
 const express = require('express');
 
+const { findByRegion, toHtml } = require('./monuments');
+
 const app = express();
 
-let monuments;
+let monumentData;
 
 fs.readFile(path.join(__dirname, '/data/firstHundred.json'), { encoding: 'utf-8' }, (err, data) => {
   if (!err) {
-    monuments = JSON.parse(data);
-    console.log(`Loaded ${monuments.length} monuments`);
+    monumentData = JSON.parse(data);
+    console.log(`Loaded ${monumentData.length} monuments`);
   } else {
     console.log(err);
   }
 });
 
-function findByRegion(obj, searchVal) {
-  const searchField = 'REG';
-  const results = [];
-  if (searchVal.length > 0) {
-    for (let i = 0; i < obj.length; i++) {
-      if (obj[i][searchField] && obj[i][searchField].toLowerCase().indexOf(searchVal.toLowerCase()) !== -1) {
-        results.push(obj[i]);
-      }
-    }
-  }
-  return results;
-}
-
-function toHtml(results) {
-  const monumentsHtml = results.map((result) => {
-    return `<div><h3>${result.TICO} - ${result.REG}</h3><p>${result.PPRO}</p></div>`;
-  }).join('');
-  return `<p>Found ${results.length} monument${results.length > 1 ? 's' : ''}</p>${monumentsHtml}`;
-}
-
 app.get('/api/search', (req, res) => {
-  res.send(toHtml(findByRegion(monuments, req.query.q)));
+  res.send(toHtml(findByRegion(monumentData, req.query.q)));
 });
 
 app.use(express.static(path.join(__dirname, '/public')));
---
+++ src/monuments.js
@@ -0,0 +1,24 @@
+function findByRegion(obj, searchVal) {
+  const searchField = 'REG';
+  const results = [];
+  if (searchVal.length > 0) {
+    for (let i = 0; i < obj.length; i++) {
+      if (obj[i][searchField] && obj[i][searchField].toLowerCase().indexOf(searchVal.toLowerCase()) !== -1) {
+        results.push(obj[i]);
+      }
+    }
+  }
+  return results;
+}
+
+function toHtml(results) {
+  const monumentsHtml = results.map((result) => {
+    return `<div><h3>${result.TICO} - ${result.REG}</h3><p>${result.PPRO}</p></div>`;
+  }).join('');
+  return `<p>Found ${results.length} monument${results.length > 1 ? 's' : ''}</p>${monumentsHtml}`;
+}
+
+module.exports = {
+  findByRegion,
+  toHtml,
+};
